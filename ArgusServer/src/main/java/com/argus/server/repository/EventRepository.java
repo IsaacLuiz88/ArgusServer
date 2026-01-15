16e098e109bab5fb60438a80223ec672265985c2
Isaac Luiz <113178368+IsaacLuiz88@users.noreply.github.com>
2026-01-15 13:21:19 -0300

    Delete ArgusServer directory

--- ArgusServer/src/main/java/com/argus/server/repository/EventRepository.java
+++
@@ -1,9 +0,0 @@
-package com.argus.server.repository;
-
-import com.argus.server.model.EventEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface EventRepository extends JpaRepository<EventEntity, Long> {
-    List<EventEntity> findByExamAndStudentOrderByTimestampDesc(String exam, String student);
-}
